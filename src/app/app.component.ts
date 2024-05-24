e8fe330292c00ccfeb02d00ddb28b43c3b091104
Marcelo Landim <marcelolandim85@hotmail.com>
2024-05-24 18:53:00 +0000

    atualizando app.component para usar currency pipe e incluidn o arquivo do logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, LOCALE_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import pt from '@angular/common/locales/pt'
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(pt)
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, HeaderComponent, HomeComponent],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'pt'}
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
