325f6d81e1f522f44ef6eb1e67026bd4734f9fff
Marcelo Landim <marcelolandim85@hotmail.com>
2024-05-24 18:54:28 +0000

    interfaces para carnes e bebidas e service para acesso a api

--- src/app/shared/services/churrascometro.service.ts
+++ src/app/shared/services/churrascometro.service.ts
@@ -1,25 +1,21 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, catchError, map, throwError } from 'rxjs'
-
-type Carne = {
-  id: string,
-  nome: string,
-  tipo: string,
-  preco_kg: number,
-  consumo_medio_adulto_g: number,
-  consumo_medio_crianca_g: number
-}
+import { Carne } from '../models/carnes.interface'
+import { Bebida } from '../models/bebidas.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChurrascometroService {
 
+  // Endereço público específico gerado pelo github codespaces. Em desenvolvimento local deve ser substituído por http://localhost:3000
   private API_URL = 'https://reimagined-space-fortnight-vp6gr5xg5xj3p6qx-3000.app.github.dev' //'http://localhost:3000'
+
 
   constructor(private hhtp: HttpClient) { }
 
+  // Apenas exemplo (dead code)
   getPrecoCarnesByName(nome: string): Observable<number> {
     return this.hhtp.get<Carne[]>(`${this.API_URL}/carnes/?nome=${nome}`)
                     .pipe(
@@ -31,6 +27,20 @@ export class ChurrascometroService {
                       )
                     )
   }
+
+
+  getCarnes(): Observable<Carne[]> {
+    return this.hhtp.get<Carne[]>(`${this.API_URL}/carnes`).pipe(
+      catchError(this.handlerError)
+    )
+  }
+
+  getBebidas(): Observable<Bebida[]> {
+    return this.hhtp.get<Bebida[]>(`${this.API_URL}/bebidas`).pipe(
+      catchError(this.handlerError)
+    )
+  }
+
 
   private handlerError(error: HttpErrorResponse): Observable<any> {
     console.log('Ocorreu um erro: ' + error);
